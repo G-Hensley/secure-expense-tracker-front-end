c542531686ad921806f84445e676b21bb3f747c4
Gavin Hensley <gavinhensley@protonmail.com>
2025-03-20 13:38:24 -0400

    Update the expense inputs

--- src/app/components/Expense.tsx
+++ src/app/components/Expense.tsx
@@ -29,8 +29,6 @@ export default function Expense({
     handleDescriptionChange(id, editedDescription);
     handleAmountChange(id, editedAmount);
     setIsEditing(false);
-    setEditedDescription(description);
-    setEditedAmount(amount);
   };
 
   const cancelEdit = () => {
